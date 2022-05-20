57d886a1525ae9ec14b93605a0350eef64a73e75
fabio <fabiosantosamorim2@gmail.com>
2022-05-20 09:03:16 -0300

    ARMAZENAMENTO STORAGE: estrutura e acessar dados

---
+++ banco de dados/firebase/5.armazenar_arquivos/1. estrutura_inicial/script.js
@@ -0,0 +1,16 @@
+// 3° PARTE: copiar as cofigurações do banco de dados do projeto
+const firebaseConfig = {
+    apiKey: "AIzaSyD-dYTiaxwADWUOu69B7jZC4_qmBz7pLSU",
+    authDomain: "projeto-teste-e5a52.firebaseapp.com",
+    projectId: "projeto-teste-e5a52",
+    storageBucket: "projeto-teste-e5a52.appspot.com",
+    messagingSenderId: "186244678707",
+    appId: "1:186244678707:web:7dc03f5de5505bf9bfcf46",
+    measurementId: "G-GNESNWR3RM"
+};
+//---------------------------------------------------------------
+// iniciar o banco de dados do projeto
+firebase.initializeApp(firebaseConfig)
+
+//criar o objeto storage para manipular os arquivos 
+const storage= firebase.storage();
