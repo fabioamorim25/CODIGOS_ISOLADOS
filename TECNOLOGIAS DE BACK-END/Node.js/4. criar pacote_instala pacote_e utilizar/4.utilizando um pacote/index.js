e4c90a531be8a3cd28c1614147011a87c051ae48
fabio <fabiosantosamorim2@gmail.com>
2022-06-28 10:27:47 -0300

    utilizar pacote

---
+++ TECNOLOGIAS DE BACK-END/Node.js/4. criar pacote_instala pacote_e utilizar/4.utilizando um pacote/index.js
@@ -0,0 +1,13 @@
+//chamar o pacote que esta na pasta calculadora 
+let calculadora= require('calculadora');
+
+
+//para chamar o metodo e uma função basta: chamar a [pasta calculadora. metodo que esta a função]
+console.log(calculadora.metodoCalculadora());
+
+
+
+
+
+// PARA CHAMAR UMA FUNÇÃO VAI SER PRECISO CHAMAR PELO TERMINAL "apenas pq foi definido assim no pacote"
+   // node index.js  nomeDaFunção  valor1 valor2
